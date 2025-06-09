7e4ae624d1a09fdc91ac1da18c623a647d7d79c3
tiarafebryanti <tiarafebrianti1702@gmail.com>
2025-06-09 21:48:59 +0700

    updateee

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,15 @@ const body = document.querySelector('body'),
     searchBtn = body.querySelector(".search-box"),
     modeSwitch = body.querySelector(".toggle-switch"),
     modeText = body.querySelector(".mode-text");
+    sidebarcontent = body.querySelector(".sidebar-content");
+    const mainContent = document.getElementById('mainContent');
 
     toggle.addEventListener("click", () => {
         sidebar.classList.toggle("close");
+    });
+
+    toggle.addEventListener("click", () => {
+        sidebarcontent.classList.toggle("close");
     });
 
     modeSwitch.addEventListener("click", () => {
